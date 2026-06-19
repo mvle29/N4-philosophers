a462b2549fff47b605966a43132a34ca42d1ccbd
mat <61627473+mvle29@users.noreply.github.com>
2026-06-19 23:13:55 +0200

    added real value : 199 410 200 200 works now

--- src/threads/actions.c
+++ src/threads/actions.c
@@ -12,9 +12,10 @@
 
 #include "philo.h"
 
-void	action_first_think2(t_params *p, int id)
+void	action_first_think2(t_params *p, t_philo *philo, int id)
 {
 	int	tt_think;
+	int	real;
 	int	impair_philo;
 	int	pair;
 
@@ -24,23 +25,31 @@ void	action_first_think2(t_params *p, int id)
 	impair_philo = id + pair;
 	tt_think = p->tt_die * 2;
 	if (p->nb_philo != 1)
-		tt_think = pair * p->tt_eat + p->tt_eat * ((impair_philo / 2) / (p->nb_philo / 2));
-	ft_usleep(tt_think);
+		tt_think = pair * p->tt_eat + p->tt_eat * (impair_philo / 2) / (p->nb_philo / 2);
+	real = tt_think - (get_time_ms() - philo->last_meal);
+	if (real > 0)
+		ft_usleep(real);
+	else
+		ft_usleep(1);
 }
 
 int	action_first_think(t_params *p, t_philo *philo, int id)
 {
 	int	stop;
+	int	real;
 
 	stop = philo_print_getstop(philo->data, id, "is thinking", 1);
 	if (!stop)
 	{
 		if (p->nb_philo % 2 == 1)
-			action_first_think2(p, id);
+			action_first_think2(p, philo, id);
 		else
 		{
+			real = p->tt_eat - (get_time_ms() - philo->last_meal);
+			if (real < 0)
+				real = 1;
 			if (id % 2 == 0)
-				ft_usleep(p->tt_eat);
+				ft_usleep(real);
 		}
 	}
 	return (!stop);
@@ -50,6 +59,7 @@ int	action_try_think(t_params *p, t_philo *philo, int id)
 {
 	int	stop;
 	int	tt_think;
+	int	real;
 
 	stop = philo_print_getstop(philo->data, id, "is thinking", 1);
 	if (!stop)
@@ -57,16 +67,18 @@ int	action_try_think(t_params *p, t_philo *philo, int id)
 		if (p->nb_philo % 2 == 0)
 		{
 			tt_think = p->tt_eat - p->tt_sleep;
-			if (tt_think < 0 || p->tt_die <= tt_think)
-				tt_think = 1;
-			ft_usleep(tt_think);
+			real = tt_think - (get_time_ms() - philo->last_meal);
+			if (real < 0 || p->tt_die <= real)
+				real = 0;
+			ft_usleep(real);
 		}
 		else
 		{
 			tt_think = p->tt_eat + p->tt_eat / 2 - p->tt_sleep;
-			if (tt_think < 0 || p->tt_die <= tt_think)
-				tt_think = 1;
-			ft_usleep(tt_think);
+			real = tt_think - (get_time_ms() - philo->last_meal);
+			if (real < 0 || p->tt_die <= real)
+				real = 0;
+			ft_usleep(real);
 		}
 	}
 	return (!stop);
